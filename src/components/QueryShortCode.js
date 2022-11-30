a965a43f8dbd7a33bd58e68cc715a865942fb936
Mwai John <mwaijohng@gmail.com>
2022-11-30 17:31:58 +0300

    type

--- src/components/QueryShortCode.js
+++ src/components/QueryShortCode.js
@@ -70,7 +70,7 @@ function QueryShortCode() {
       <div className='mb-6'>
         <form className='w-96' onSubmit={handleShortCodeLookUp}>
           <div className="my-2">
-            <label for="account" className="block mb-2 text-sm font-medium text-gray-900">Enter Customer Account</label>
+            <label for="account" className="block mb-2 text-sm font-medium text-gray-900">Enter Short Code</label>
             <input type="text" id="account" name='account' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
           </div>
           <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Query Short Code</button>
